4a43f09493ebf45bc30cb5e63afa17a5df447eea
Fabio-TecnologyInnovation <fabiomeiramv@hotmail.com>
2022-10-11 15:34:23 -0300

    Projeto alterado

--- RelogioDigital.py
+++ RelogioDigital.py
@@ -22,16 +22,11 @@ def relogio():
     #VARIÁVEIS
     tempo = datetime.now()
     hora = tempo.strftime('%H:%M:%S')
-    dia_semana = tempo.strftime('%A')
-    dia = tempo.day
-    mes = tempo.strftime('%B') # %b , minúsculo abrevia o nome dos meses
-    ano = tempo.strftime('%Y')
+    data = tempo.strftime('%A %d/%B/%Y')
 
     l1.config(text=hora)
     l1.after(200, relogio)
-    l2.config(text=dia_semana + ' ' + str(dia) + '/' + 
-                            str(mes) + '/' + str(ano))
-
+    l2.config(text=data)
 
 l1 = Label(janela, text='', font=('Arial 80'), bg=cor1, fg=cor3)
 l1.grid(row=0, column=0, sticky=NW, padx=5)
